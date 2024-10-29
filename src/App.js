9a6ea32dd67b9ec65189dfc7b41d1ed080a1b3ea
antonia-reyes <antonia.reyes@uc.cl>
2024-10-29 10:44:14 -0300

    feat: add central picture, needs, adjustment

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <img src={`${process.env.PUBLIC_URL}/fondo3.png`} alt="Imagen central" className="central-image" />
     </div>
   );
 }
